3d25983f42fd38a4f4bc467ff7c96926434aa61b
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-10-24 23:27:25 -0600

    [UPDATE] Generate QR Button atom completed

--- app/src/main/java/com/app/arcabyolimpo/presentation/ui/components/atoms/buttons/GenerateQR.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/ui/components/atoms/buttons/GenerateQR.kt
@@ -1,32 +1,59 @@
-package com.app.arcabyolimpo.presentation.ui.components.atoms
+package com.app.arcabyolimpo.presentation.ui.components.atoms.buttons
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.app.arcabyolimpo.presentation.theme.Poppins
 
 @Composable
 fun GenerateQrButton(
-    text: String,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier,
+    cornerRadius: Dp = 8.dp,
+    width: Dp = 88.dp,
+    height: Dp = 32.dp,
 ){
     Button(
         onClick = onClick,
-        modifier = modifier.size(width = 88.dp, height = 32.dp),
+        modifier = Modifier.size(width = width, height = height),
         colors = ButtonDefaults.buttonColors(
-            containerColor = Color(0xFFFFF7EB),
-            contentColor = Color(0xFF2844AE)
+            containerColor = Color(0xFFFFF7EB)),
+        shape = RoundedCornerShape(cornerRadius),
+        contentPadding = PaddingValues(
+            horizontal = 7.dp,
+            vertical = 6.dp
         ),
+
     ){
         Text(
-            text = text,
+            text = "Generar QR",
+            color = Color(0xFF2844AE),
+            fontFamily = Poppins,
+            fontWeight = FontWeight.SemiBold,
             fontSize = 12.sp
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+)
+@Composable
+fun GenerateQrButtonPreview() {
+    MaterialTheme {
+        GenerateQrButton(
+            onClick = { },
         )
     }
 }
