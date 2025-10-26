1811df5e336195897f7cfe641777a04541dda512
EmiliaCardenas <a01706807@tec.mx>
2025-10-26 11:15:16 -0600

    [ADD] adding status atoms

---
+++ app/src/main/java/com/app/arcabyolimpo/presentation/ui/components/atoms/status/EncursoStatus.kt
@@ -0,0 +1,49 @@
+package com.app.arcabyolimpo.presentation.ui.components.atoms.status
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun EncursoStatus(
+    modifier: Modifier = Modifier.padding(16.dp),
+    width: Dp = 95.dp,
+    height: Dp = 24.dp,
+    backgroundColor: Color = Color(0xFFFFF7EB),
+    text: String = "En curso",
+    textColor: Color = Color(0xFF2844AE),
+    cornerRadius: Dp = 8.dp,
+    fontSize: Int = 12,
+    fontFamily: FontFamily = Poppins,
+) {
+    Box(
+        modifier =
+            modifier
+                .size(width = width, height = height)
+                .clip(RoundedCornerShape(cornerRadius))
+                .background(backgroundColor),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            fontSize = fontSize.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = fontFamily,
+        )
+    }
+}
