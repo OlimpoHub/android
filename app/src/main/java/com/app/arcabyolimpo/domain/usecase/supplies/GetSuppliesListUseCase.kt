150a84b5e96ba6d681c2f9502b5a93bd78987cce
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-11-05 01:26:43 -0600

    [ADD] data layer for supplies module

---
+++ app/src/main/java/com/app/arcabyolimpo/domain/usecase/supplies/GetSuppliesListUseCase.kt
@@ -0,0 +1,25 @@
+package com.app.arcabyolimpo.domain.usecase.supplies
+
+import com.app.arcabyolimpo.domain.common.Result
+import com.app.arcabyolimpo.domain.model.supplies.Supply
+import com.app.arcabyolimpo.domain.repository.supplies.SupplyRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetSuppliesListUseCase
+    @Inject
+    constructor(
+        private val repository: SupplyRepository
+) {
+    operator fun invoke(): Flow<Result<List<Supply>>> =
+        flow {
+            try{
+                emit(Result.Loading)
+                val supplies = repository.getSuppliesList()
+                emit(Result.Success(supplies))
+            } catch (e: Exception){
+                emit(Result.Error(e))
+            }
+    }
+}
