4bdeb7e3b3878ee6433ba1c058581190b40829c9
Yessica <a01273574@tec.mx>
2025-11-08 16:41:35 -0600

    [ADD] Decision Dialog and Snackbar front logic

--- app/src/main/java/com/app/arcabyolimpo/presentation/ui/components/atoms/status/CompletedStatus.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/ui/components/atoms/status/CompletedStatus.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.app.arcabyolimpo.presentation.theme.Poppins
 
 @Suppress("ktlint:standard:function-naming")
 @Composable
