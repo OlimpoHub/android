ea372f81998e97f3476d1c158102c2cb04f17034
EmiliaCardenas <a01706807@tec.mx>
2025-11-08 17:21:34 -0600

    [ADD] Add workshops Use cases

--- app/src/main/java/com/app/arcabyolimpo/domain/usecase/workshops/GetWorkshopUseCase.kt
+++ app/src/main/java/com/app/arcabyolimpo/domain/usecase/workshops/GetWorkshopUseCase.kt
@@ -1,4 +1,25 @@
 package com.app.arcabyolimpo.domain.usecase.workshops
 
-class GetWorkshopUseCase {
-}
+import com.app.arcabyolimpo.domain.common.Result
+import com.app.arcabyolimpo.domain.model.workshops.Workshop
+import com.app.arcabyolimpo.domain.repository.workshops.WorkshopRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetWorkshopUseCase
+    @Inject
+    constructor(
+        private val repository: WorkshopRepository
+    ) {
+    operator fun invoke(id: String): Flow<Result<Workshop>> =
+        flow {
+            try {
+                emit(Result.Loading)
+                val workshop = repository.getWorkshopsById(id)
+                emit(Result.Success(workshop))
+            } catch (e: Exception) {
+                emit(Result.Error(e))
+            }
+        }
+    }
--- app/src/main/java/com/app/arcabyolimpo/domain/usecase/workshops/GetWorkshopsListUseCase.kt
+++ app/src/main/java/com/app/arcabyolimpo/domain/usecase/workshops/GetWorkshopsListUseCase.kt
@@ -1,4 +1,25 @@
 package com.app.arcabyolimpo.domain.usecase.workshops
 
-class GetWorkshopsListUseCase {
-}
+import com.app.arcabyolimpo.domain.common.Result
+import com.app.arcabyolimpo.domain.model.workshops.Workshop
+import com.app.arcabyolimpo.domain.repository.workshops.WorkshopRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetWorkshopsListUseCase
+    @Inject
+    constructor(
+        private val repository: WorkshopRepository
+    ) {
+    operator fun invoke(id: String): Flow<Result<List<Workshop>>> =
+        flow {
+            try {
+                emit(Result.Loading)
+                val workshops = repository.getWorkshopsList()
+                emit(Result.Success(workshops))
+            } catch (e: Exception) {
+                emit(Result.Error(e))
+            }
+        }
+    }
