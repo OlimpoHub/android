671aef86a1f0b6b5f00f07ba253c7edeec7e9813
Rigby119 <rigby119@gmail.com>
2025-11-12 02:44:09 -0600

    [FIX] Move Molecule to right folder

--- app/src/main/java/com/app/arcabyolimpo/presentation/ui/components/molecules/ProductBatchItem.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/ui/components/molecules/ProductBatchItem.kt
@@ -1,12 +1,7 @@
-package com.app.arcabyolimpo.presentation.screens.productbatches.productBatchesList.components
+package com.app.arcabyolimpo.presentation.ui.components.molecules
 
-import android.R
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,20 +11,13 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -38,7 +26,6 @@ import coil.compose.AsyncImage
 import com.app.arcabyolimpo.presentation.screens.productbatches.model.ProductBatchUiModel
 import com.app.arcabyolimpo.presentation.ui.components.atoms.buttons.ViewButton
 import com.app.arcabyolimpo.ui.theme.Background
-import com.app.arcabyolimpo.ui.theme.ButtonBlue
 import com.app.arcabyolimpo.ui.theme.DangerGray
 import com.app.arcabyolimpo.ui.theme.White
 
