e90f7c5fcacbdecaf0ced81fb869cb493dee7af0
YaelChar417 <A01711111@tec.mx>
2025-11-12 17:36:46 -0600

    [ADD] ViewModel for the US addSupply

--- app/src/main/java/com/app/arcabyolimpo/presentation/screens/supply/supplyAdd/SupplyAddViewModel.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/screens/supply/supplyAdd/SupplyAddViewModel.kt
@@ -1,4 +1,93 @@
 package com.app.arcabyolimpo.presentation.screens.supply.supplyAdd
 
-class SupplyAddViewModel {
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.app.arcabyolimpo.domain.model.supplies.SupplyAdd
+import com.app.arcabyolimpo.domain.usecase.supplies.GetWorkshopCategoryInfoUseCase
+import com.app.arcabyolimpo.domain.usecase.supplies.AddSupplyUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class SupplyAddViewModel @Inject constructor(
+    private val getWorkshopCategoryInfoUseCase: GetWorkshopCategoryInfoUseCase,
+    private val addSuppliesUseCase: AddSupplyUseCase,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(SupplyAddUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        loadDropDownData()
+    }
+
+    private fun loadDropDownData() {
+        viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true) }
+            val result = getWorkshopCategoryInfoUseCase()
+
+            _uiState.update {
+                if (result.isSuccess) {
+                    val data = result.getOrNull()
+                    it.copy(
+                        isLoading = false,
+                        workshops = data?.workshops ?: emptyList(),
+                        categories = data?.categories ?: emptyList(),
+                    )
+                }else {
+                    it.copy(
+                        isLoading = false,
+                        error = "Error cargando datos",
+                    )
+                }
+            }
+        }
+    }
+
+    fun onNameChange(name: String) { _uiState.update { it.copy(name = name) } }
+    fun onUnitMeasureChange(measureUnit: String) { _uiState.update { it.copy(measureUnit = measureUnit) } }
+    fun onImageSelected(uri: Uri?) { _uiState.update { it.copy(selectedImage = uri) } }
+    fun onStatusChange(newStatus: Int) { _uiState.update { it.copy(status = newStatus) } }
+    fun onWorkshopSelected(idWorkshop: String) { _uiState.update { it.copy(selectedWorkshop = idWorkshop) } }
+    fun onCategorySelected(idCategory: String) { _uiState.update { it.copy(selectedCategory = idCategory) } }
+    fun onSaveClick() {
+        val state = _uiState.value
+
+        if (state.name.isBlank() || state.selectedWorkshop == null || state.selectedCategory == null) {
+            _uiState.update { it.copy(error = "Completa todos los campos") }
+            return
+        }
+
+        viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true) }
+
+            val supply = SupplyAdd(
+                idWorkshop = state.selectedWorkshop,
+                name = state.name,
+                measureUnit = state.measureUnit,
+                idCategory = state.selectedCategory,
+                status = state.status
+            )
+
+            val result = addSuppliesUseCase(supply, state.selectedImage)
+
+            _uiState.update {
+                if (result.isSuccess) {
+                    it.copy(
+                        isLoading = false,
+                        success = true,
+                    )
+                } else {
+                    it.copy(
+                        isLoading = false,
+                        error = result.exceptionOrNull()?.message
+                    )
+                }
+            }
+        }
+    }
 }
+
