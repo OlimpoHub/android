531550f68a0938f06826ede6489c038d8ae32caa
Yael Charles <yaelcharles417@gmail.com>
2025-11-12 22:43:16 -0600

    [ADD] SupplyAddScreen Finished

--- app/src/main/java/com/app/arcabyolimpo/presentation/screens/supply/supplyAdd/SupplyAddViewModel.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/screens/supply/supplyAdd/SupplyAddViewModel.kt
@@ -47,16 +47,28 @@ class SupplyAddViewModel @Inject constructor(
         }
     }
 
-    fun onNameChange(name: String) { _uiState.update { it.copy(name = name) } }
-    fun onUnitMeasureChange(measureUnit: String) { _uiState.update { it.copy(measureUnit = measureUnit) } }
-    fun onImageSelected(uri: Uri?) { _uiState.update { it.copy(selectedImage = uri) } }
-    fun onStatusChange(newStatus: Int) { _uiState.update { it.copy(status = newStatus) } }
-    fun onWorkshopSelected(idWorkshop: String) { _uiState.update { it.copy(selectedWorkshop = idWorkshop) } }
-    fun onCategorySelected(idCategory: String) { _uiState.update { it.copy(selectedCategory = idCategory) } }
+    fun onNameChange(name: String) {
+        _uiState.update { it.copy(name = name) }
+    }
+    fun onUnitMeasureChange(measureUnit: String) {
+        _uiState.update { it.copy(measureUnit = measureUnit) }
+    }
+    fun onImageSelected(uri: Uri?) {
+        _uiState.update { it.copy(selectedImage = uri) }
+    }
+    fun onStatusChange(newStatus: Int) {
+        _uiState.update { it.copy(status = newStatus) }
+    }
+    fun onWorkshopSelected(idWorkshop: String) {
+        _uiState.update { it.copy(selectedWorkshopId = idWorkshop) }
+    }
+    fun onCategorySelected(idCategory: String) {
+        _uiState.update { it.copy(selectedCategoryId = idCategory) }
+    }
     fun onSaveClick() {
         val state = _uiState.value
 
-        if (state.name.isBlank() || state.selectedWorkshop == null || state.selectedCategory == null) {
+        if (state.name.isBlank() || state.selectedWorkshopId == null || state.selectedCategoryId == null) {
             _uiState.update { it.copy(error = "Completa todos los campos") }
             return
         }
@@ -65,10 +77,10 @@ class SupplyAddViewModel @Inject constructor(
             _uiState.update { it.copy(isLoading = true) }
 
             val supply = SupplyAdd(
-                idWorkshop = state.selectedWorkshop,
+                idWorkshop = state.selectedWorkshopId,
                 name = state.name,
                 measureUnit = state.measureUnit,
-                idCategory = state.selectedCategory,
+                idCategory = state.selectedCategoryId,
                 status = state.status
             )
 
