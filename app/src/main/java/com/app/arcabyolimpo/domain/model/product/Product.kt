bbabf33bfb2ff610dedfe5d75b2d3efcc940319d
Nicole Uriostegui <a01711853@tec.mx>
2025-11-17 13:59:48 -0600

    [FIX] merge conflict

---
+++ app/src/main/java/com/app/arcabyolimpo/domain/model/product/Product.kt
@@ -0,0 +1,15 @@
+package com.app.arcabyolimpo.domain.model.product
+
+/**
+ * Domain model para mostrar / listar productos en la app.
+ * NO reemplaza ProductAdd, lo complementa.
+ */
+data class Product(
+    val id: String,              // idProducto
+    val name: String,            // Nombre
+    val unitaryPrice: String,    // PrecioUnitario
+    val workshopName: String?,   // nombreTaller
+    val description: String?,    // Descripcion
+    val available: Boolean,      // Disponible == 1 -> true
+    val imageUrl: String?,       // imagen
+)
