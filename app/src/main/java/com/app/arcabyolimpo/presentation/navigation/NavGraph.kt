d9963a305d961b191b2d209080306bab72650dc3
Nicole Uriostegui <a01711853@tec.mx>
2025-11-18 22:16:16 -0600

    [UPDATE] startDestination

--- app/src/main/java/com/app/arcabyolimpo/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/navigation/NavGraph.kt
@@ -180,7 +180,7 @@ fun ArcaNavGraph(
     NavHost(
         navController = navController,
         // TODO: Cambiar a Screen.Splash.route cuando acabe
-        startDestination = Screen.ProductUpdate.route,
+        startDestination = Screen.Splash.route,
         modifier = modifier,
     ) {
         /** Splash Screen */
