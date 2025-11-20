b5ae4428d8cb42c71498eefc488d6b3c80fbf5db
Ale Arredondo <alejandrarredondo09@gmail.com>
2025-11-20 16:37:42 -0600

    [FIX] horizontal scroll

--- app/src/main/java/com/app/arcabyolimpo/presentation/screens/supply/supplyBatchList/SupplyBatchList.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/screens/supply/supplyBatchList/SupplyBatchList.kt
@@ -1,9 +1,14 @@
 package com.app.arcabyolimpo.presentation.screens.supply.supplyBatchList
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.AlertDialogDefaults.containerColor
@@ -19,13 +24,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -99,16 +102,17 @@ fun SupplyBatchListScreen(
     ) { padding ->
         val configuration = LocalConfiguration.current
         val isLandscape = configuration.screenWidthDp > configuration.screenHeightDp
-        val scrollModifier = if (isLandscape) Modifier.verticalScroll(rememberScrollState()) else Modifier
+        val scrollModifier =
+            if (isLandscape) Modifier.horizontalScroll(rememberScrollState()) else Modifier
 
         Box(
-                modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(padding)
-                        .padding(8.dp)
-                        .then(scrollModifier),
-            ) {
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .padding(8.dp)
+                    .then(scrollModifier),
+        ) {
             SupplyBatchListContent(
                 uiState = state,
                 date = date,
