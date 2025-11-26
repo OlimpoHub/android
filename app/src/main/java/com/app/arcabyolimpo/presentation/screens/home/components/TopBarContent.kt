548f379fd6d7f723cfbb6dd0dee726ba5bf503a2
Jorge Garzon <A01707102@tec.mx>
2025-11-26 11:30:11 -0600

    [ADD] logout confirmation dialog

--- app/src/main/java/com/app/arcabyolimpo/presentation/screens/home/components/TopBarContent.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/screens/home/components/TopBarContent.kt
@@ -11,14 +11,16 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.app.arcabyolimpo.presentation.screens.session.SessionViewModel
-import com.app.arcabyolimpo.presentation.ui.components.atoms.icons.ConfigurationIcon
+import com.app.arcabyolimpo.presentation.ui.components.atoms.alerts.DecisionDialog
 import com.app.arcabyolimpo.presentation.ui.components.atoms.icons.LogoutIcon
-import com.app.arcabyolimpo.presentation.ui.components.atoms.icons.NotificationIcon
 
 /**
  * Displays the main top bar for the Home screen, showing a welcome message,
@@ -30,9 +32,25 @@ import com.app.arcabyolimpo.presentation.ui.components.atoms.icons.NotificationI
 @Composable
 fun TopBarContent(sessionViewModel: SessionViewModel = hiltViewModel()) {
     val username by sessionViewModel.username.collectAsState(initial = "")
-    val role by sessionViewModel.role.collectAsState(initial = "")
+
+    var showLogoutDialog by remember { mutableStateOf(false) }
 
     Column(modifier = Modifier.fillMaxWidth()) {
+
+        if (showLogoutDialog) {
+            DecisionDialog(
+                onDismissRequest = { showLogoutDialog = false },
+                onConfirmation = {
+                    showLogoutDialog = false
+                    sessionViewModel.logout()
+                },
+                dialogTitle = "Cerrar sesión",
+                dialogText = "¿Estás seguro de que deseas cerrar sesión?",
+                confirmText = "Confirmar",
+                dismissText = "Cancelar"
+            )
+        }
+
         TopAppBar(
             title = {
                 Column {
@@ -55,7 +73,7 @@ fun TopBarContent(sessionViewModel: SessionViewModel = hiltViewModel()) {
                     containerColor = Color(0xFF040610),
                 ),
             actions = {
-                IconButton(onClick = { sessionViewModel.logout() }) {
+                IconButton(onClick = { showLogoutDialog = true  }) {
                     LogoutIcon()
                 }
             },
