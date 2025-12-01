9c4036eb2a84f233e47270ac9c6175580d699be8
EmiliaCardenas <a01706807@tec.mx>
2025-12-01 08:39:18 -0600

    [FIX] fix last commit

--- app/src/main/java/com/app/arcabyolimpo/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/navigation/NavGraph.kt
@@ -193,6 +193,7 @@ sealed class Screen(
  * @param navController The controller managing app navigation.
  * @param sessionManager Observes session state to handle automatic logout or token expiration.
  */
+ 
 @Suppress("ktlint:standard:function-naming")
 @Composable
 fun ArcaNavGraph(
