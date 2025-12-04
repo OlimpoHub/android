a25b7a505ede447ee9abc53cc92586aa7e870ea1
XMaster417 <149627661+YaelChar417@users.noreply.github.com>
2025-12-04 14:34:57 -0600

    Standard fixed of comments

--- app/src/main/java/com/app/arcabyolimpo/presentation/screens/supply/supplyBatchList/SupplyBatchListUiState.kt
+++ app/src/main/java/com/app/arcabyolimpo/presentation/screens/supply/supplyBatchList/SupplyBatchListUiState.kt
@@ -2,10 +2,10 @@ package com.app.arcabyolimpo.presentation.screens.supply.supplyBatchList
 
 import com.app.arcabyolimpo.domain.model.supplies.SupplyBatchList
 
-/** ---------------------------------------------------------------------------------------------- *
+/**
  * SupplyBatchListUiState -> Data class with states that the view can have, such as the loding
  * state or an error had happen.
- * ---------------------------------------------------------------------------------------------- */
+ */
 data class SupplyBatchListUiState(
     val supplyBatchList: SupplyBatchList? = null,
     val supplyName: String? = null,
